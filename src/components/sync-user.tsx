58d2d701646b15699c4d8898ceba3d29d96568f2
yuriipidlisnyi <yuriipidlisnyi2021@gmail.com>
2024-10-27 20:22:34 +0200

    fix: infite upserts of the user on page reload

--- src/components/sync-user.tsx
+++ src/components/sync-user.tsx
@@ -2,22 +2,49 @@
 
 import { useUser } from "@clerk/nextjs";
 import { useEffect } from "react";
+import { useSessionStorage } from "usehooks-ts";
+
 import { api } from "~/trpc/react";
+
+const SESSION_STORAGE_IS_USER_SYNCED_KEY = "IS_USER_SYNCED";
+
 
 export const SyncUser = () => {
   const user = useUser();
-  const mutate = api.user.upsert.useMutation();
+
+  const [hasSynced, setHasSynced] = useSessionStorage<boolean>(
+    SESSION_STORAGE_IS_USER_SYNCED_KEY,
+    false,
+    {},
+  );
+
+  const mutate = api.user.upsert.useMutation({
+    retry: false,
+    onSuccess: () => {
+      setHasSynced(true);
+    },
+  });
 
   useEffect(() => {
-    if (!user.isLoaded || !user.isSignedIn) return;
+    if (!user.isLoaded || !user.isSignedIn || mutate.isPending || hasSynced)
+      return;
 
     const { user: userData } = user;
+
     mutate.mutate({
       clerkId: userData.id,
-      username: userData.username ?? `${userData.firstName} ${userData.lastName}`,
-      email: userData.primaryEmailAddress?.emailAddress ?? userData.emailAddresses?.[0]?.emailAddress ?? "",
-    })
-  }, [user]);
+      username:
+        userData.username ?? `${userData.firstName} ${userData.lastName}`,
+      email:
+        userData.primaryEmailAddress?.emailAddress ??
+        userData.emailAddresses?.[0]?.emailAddress ??
+        "",
+    });
+
+    return () => {
+      setHasSynced(true);
+    };
+  }, [user, mutate, hasSynced, setHasSynced]);
 
   return null;
 };
