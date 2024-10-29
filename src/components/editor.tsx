46d9002364597b44f3e900afb8927d5734b94a9e
yuriipidlisnyi <yuriipidlisnyi2021@gmail.com>
2024-10-29 23:06:50 +0200

    fix: rm preview

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,24 +1,16 @@
 "use client";
 
-import { type FC, useState } from "react";
+import { type FC } from "react";
 import Editor from "react-simple-wysiwyg";
 import { useForm, Controller } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { BlogContent } from "~/components/blog-content";
-import { Switch } from "~/components/ui/switch";
 import { Label } from "~/components/ui/label";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 import { api } from "~/trpc/react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
-
-
-enum pageMode {
-  view = "view",
-  edit = "edit",
-}
 
 const MS_TO_REDIRECT = 2000;
 const msToSeconds = (ms: number) => ms / 1000;
@@ -42,7 +34,6 @@ type Props = {
   userId: string;
 };
 export const EditorWYSIWYF: FC<Props> = ({ userId }) => {
-  const [mode, setMode] = useState<pageMode>(pageMode.edit);
   const router = useRouter();
   const { mutateAsync } = api.post.create.useMutation({
     onSuccess: ({ slug }) => {
@@ -59,7 +50,6 @@ export const EditorWYSIWYF: FC<Props> = ({ userId }) => {
     handleSubmit,
     control,
     formState: { errors },
-    getValues,
   } = useForm<FormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -84,65 +74,42 @@ export const EditorWYSIWYF: FC<Props> = ({ userId }) => {
     });
   };
 
-  const onModeChange = () => {
-    setMode((prevState) => {
-      return prevState === pageMode.view ? pageMode.edit : pageMode.view;
-    });
-  };
-
   return (
     <div>
-      <div className="mb-8 flex items-center space-x-2">
-        <Switch
-          id="edit-mode"
-          checked={mode === pageMode.edit}
-          onClick={onModeChange}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid w-full items-center gap-1.5"
+      >
+        <Label htmlFor="slug">Slug</Label>
+        <Input id="slug" type="text" {...register("slug")} />
+        {errors.slug && <p className="text-red-500">{errors.slug.message}</p>}
+
+        <Label htmlFor="title">Title</Label>
+        <Input id="title" type="text" {...register("title")} />
+        {errors.title && <p className="text-red-500">{errors.title.message}</p>}
+
+        <Label htmlFor="brief">Brief</Label>
+        <Input id="brief" type="text" {...register("brief")} />
+        {errors.brief && <p className="text-red-500">{errors.brief.message}</p>}
+
+        <Label htmlFor="content">Content</Label>
+        <Controller
+          name="content"
+          control={control}
+          render={({ field }) => (
+            <Editor
+              id="content"
+              value={field.value}
+              onChange={field.onChange}
+            />
+          )}
         />
-        <Label htmlFor="edit-mode">{mode}</Label>
-      </div>
+        {errors.content && (
+          <p className="text-red-500">{errors.content.message}</p>
+        )}
 
-      {mode === pageMode.view ? (
-        <BlogContent content={getValues("content")} isShowComments={false}/>
-      ) : (
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="grid w-full items-center gap-1.5"
-        >
-          <Label htmlFor="slug">Slug</Label>
-          <Input id="slug" type="text" {...register("slug")} />
-          {errors.slug && <p className="text-red-500">{errors.slug.message}</p>}
-
-          <Label htmlFor="title">Title</Label>
-          <Input id="title" type="text" {...register("title")} />
-          {errors.title && (
-            <p className="text-red-500">{errors.title.message}</p>
-          )}
-
-          <Label htmlFor="brief">Brief</Label>
-          <Input id="brief" type="text" {...register("brief")} />
-          {errors.brief && (
-            <p className="text-red-500">{errors.brief.message}</p>
-          )}
-
-          <Label htmlFor="content">Content</Label>
-          <Controller
-            name="content"
-            control={control}
-            render={({ field }) => (
-              <Editor
-                id="content"
-                value={field.value}
-                onChange={field.onChange}
-              />
-            )}
-          />
-          {errors.content && (
-            <p className="text-red-500">{errors.content.message}</p>
-          )}
-
-          <Button type="submit">Save</Button>
-        </form>
-      )}
+        <Button type="submit">Save</Button>
+      </form>
     </div>
   );
 };
