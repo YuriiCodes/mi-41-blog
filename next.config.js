d9564e097690515ef5c318aececd06efa33d5d39
yuriipidlisnyi <yuriipidlisnyi2021@gmail.com>
2024-11-02 01:09:51 +0200

    feat: add docs

--- next.config.js
+++ next.config.js
@@ -4,7 +4,18 @@
  */
 await import("./src/env.js");
 
-/** @type {import("next").NextConfig} */
-const config = {};
+import createMDX from '@next/mdx'
 
-export default config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
